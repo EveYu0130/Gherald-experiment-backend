cbac4291a3e843b6b0adc3d0281c63823db77eff
Xueyao Yu <x224yu@v1040-wn-rt-a-11-214.campus-dynamic.uwaterloo.ca>
2022-12-01 15:48:27 -0500

    revert change

--- src/main/java/com/gherald/springboot/SpringbootApplication.java
+++ src/main/java/com/gherald/springboot/SpringbootApplication.java
@@ -2,12 +2,9 @@ package com.gherald.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -18,14 +15,9 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/*").allowedOrigins("http://swag.cs.uwaterloo.ca");
-			}
-		};
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
